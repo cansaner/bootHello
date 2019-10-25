f760c370b46b33b0112d8206aad77ea807e255d1
cansaner <cansaner@gmail.com>
2019-10-25 17:06:05 +0200

    Changed message

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Can Saner!";
+        return "Hello from Can Saner! How are you?";
     }
     
 }
